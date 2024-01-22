1cde3b68bcbcf261f2e7a042e152eed0bade21d9
Shubham-code4each <shubham@code4each.com>
2024-01-22 16:19:57 +0530

    minor changes

--- functions/getSymptoms.php
+++ functions/getSymptoms.php
@@ -19,6 +19,8 @@ if (isset($_POST['diagnosisid'])) {
         $result = $db->query($query)->fetchAll();
         if (count($result) > 0) {
             echo json_encode($result);
+        }else{
+            echo json_encode([]);
         }
     }
 }
